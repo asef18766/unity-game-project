068a0c2e9b4c1bd1e38c38e4fea968417064b423
asef18766 <asef18766@gmail.com>
2019-03-28 11:56:41 +0800

    complete the full basic structure

---
+++ underworld/Assets/camera_operation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_operation : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject tar;
+	Transform tf;
+	void Start () {
+		tf=GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camera_follow();
+	}
+	void camera_follow()
+	{
+		GetComponent<Transform>().position=new Vector3(tar.GetComponent<Transform>().position.x,
+												   	   tar.GetComponent<Transform>().position.y,
+														   GetComponent<Transform>().position.z);
+	}
+}
