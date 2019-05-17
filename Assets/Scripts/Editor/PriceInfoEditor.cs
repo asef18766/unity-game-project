f183b5645fda1af8532950a1361af89475ebd040
Bogay <pojay11523@gmail.com>
2019-05-17 17:42:05 +0800

    add price system, manage products name and price

---
+++ Assets/Scripts/Editor/PriceInfoEditor.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PriceInfo))]
+public class PriceInfoEditor : Editor
+{
+	private class Item
+	{
+		public string key;
+		public int val;
+
+		public Item(string k, int v)
+		{
+			key = k;
+			val = v;
+		}
+	}
+
+	private PriceInfo info;
+	private string newKey;
+	private int newVal;
+
+	void OnEnable()
+	{
+		info = target as PriceInfo;	
+		this.newKey = "";
+		this.newVal = 0;
+	}
+
+	private void modifyDict(List<string> ks, string k, List<int> vs, int v)
+	{
+		string bk = k; // backup for old key
+		EditorGUILayout.BeginHorizontal();
+		EditorGUI.BeginChangeCheck();
+		k = EditorGUILayout.TextField(k, GUILayout.Width(80));
+		// key was modified
+		if(EditorGUI.EndChangeCheck())
+		{
+			v = EditorGUILayout.IntField(v, GUILayout.Width(80));
+			if(ks.Contains(k))
+			{
+				k += "(duplicate key)";
+			}
+			else
+			{
+				int i = ks.IndexOf(bk);
+				ks[i] = k;
+				vs[i] = v;
+			}
+		}
+		else
+		{
+			v = EditorGUILayout.IntField(v, GUILayout.Width(80));
+			int i = ks.IndexOf(bk);
+			ks[i] = k;
+			vs[i] = v;
+		}
+		// delete product
+		if(GUILayout.Button("-", GUILayout.Width(40)))
+		{
+			int i = ks.IndexOf(k);
+			ks.RemoveAt(i);
+			vs.RemoveAt(i);
+		}
+		EditorGUILayout.EndHorizontal();
+	}
+
+	public override void OnInspectorGUI()
+	{
+		// dump kv-pair to list
+		List<Item> items = new List<Item>();
+		for(int i=0, l=info.keys.Count ; i<l ; i++)
+		{
+			Item kv = new Item(info.keys[i], info.vals[i]);
+			items.Add(kv);
+		}
+
+		// draw on inspector
+		int len = items.Count;
+		for(int i=0 ; i<len ; i++)
+		{
+			this.modifyDict(info.keys, items[i].key, info.vals, items[i].val);
+		}
+
+		GUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("Add New Item");
+		GUILayout.EndHorizontal();
+		this.newKey = EditorGUILayout.TextField(this.newKey);
+		this.newVal = EditorGUILayout.IntField(this.newVal);
+		if(GUILayout.Button("+"))
+		{
+			if(info.keys.Contains(newKey))
+			{
+				GUI.color = Color.red;
+				EditorGUILayout.LabelField("dunplicate key!");
+				GUI.color = Color.black;
+			}
+			else
+			{
+				info.keys.Add(newKey);
+				info.vals.Add(newVal);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PriceInfo.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// use for store object name and price
+[CreateAssetMenu()]
+public class PriceInfo : ScriptableObject {
+	public List<string> keys;
+	public List<int> vals;
+
+	public Dictionary<string, int> price { get; private set; }
+	
+	private void OnEnable()
+	{
+		this.price = new Dictionary<string, int>();
+		for(int i=0, l=this.keys.Count ; i<l ; i++) this.price.Add(this.keys[i], this.vals[i]);
+	}
+
+	public void onBuy(string name, int count)
+	{
+		if(!this.price.ContainsKey(name))
+		{
+			Debug.Log("Error: the item [" + name + "] not found!");
+			return;
+		}
+
+		int old = price[name];
+		price[name] += (int)(count / old * Random.Range(0.8f, 1.2f));
+	}
+
+	public void onSell(string name, int count)
+	{
+		if(!this.price.ContainsKey(name))
+		{
+			Debug.Log("Error: the item [" + name + "] not found!");
+			return;
+		}
+
+		int old = price[name];
+		price[name] -= (int)(count / old * Random.Range(0.8f, 1.2f));
+		price[name] = Mathf.Max(price[name], 5);
+	}
+
+	// call this to dump the dict value into list (for serialize)
+	public void save()
+	{
+		this.keys.Clear();
+		this.vals.Clear();
+
+		foreach(var kv in this.price)
+		{
+			this.keys.Add(kv.Key);
+			this.vals.Add(kv.Value);
+		}
+	}
+}
