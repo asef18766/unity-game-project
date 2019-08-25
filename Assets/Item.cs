ccc831678ceeb899edb916cff4858963e3ab0699
asef18766 <asef18766@gmail.com>
2019-08-25 16:06:56 +0800

    add item object & fixed Player Script's coding style

---
+++ Assets/Item.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	[SerializeField] int id=-1;
+	[SerializeField][TextArea] string help_text;
+	[SerializeField] I_ItemUsage usage=null;
+	public Item(Item i)
+	{
+		int id=i.getId();
+		help_text=i.get_help_text();
+		usage=i.usage;
+
+	}
+	void Start()
+	{
+		if(id==-1)
+			Debug.Log("item id does not assigned");
+	}
+	public bool isUsable(){ return usage!=null; }
+	public int getId(){ return id; }
+	public string get_help_text(){ return help_text; }
+	public void use_item()
+	{
+		usage.use();
+	}
+
+	public static GameObject InstatiateItem(GameObject item)
+	{
+		GameObject n_item=Instantiate(item);
+		foreach(Behaviour behaviour in n_item.GetComponentsInChildren<Behaviour>())
+    		behaviour.enabled = true;
+		return n_item;
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,140 @@
 using System;
 using UnityEngine;
 public class Player : Entity {
-	// Use this for initialization
-	public enum  walk_dir{UP,DOWN,LEFT,RIGHT};
+	#region walking_behavior
+
+		public enum  walk_dir{UP,DOWN,LEFT,RIGHT};
+		Vector2 moving_dir;
+		walk_dir[] playerControler()
+		{
+			walk_dir[] ret=new walk_dir[4];
+			int element=0;
+			if(Input.GetKey(KeyCode.A))
+			{
+				ret[element]=walk_dir.LEFT;
+				element++;
+			}
+			if(Input.GetKey(KeyCode.W))
+			{
+				ret[element]=walk_dir.UP;
+				element++;
+			}
+			if(Input.GetKey(KeyCode.D))
+			{
+				ret[element]=walk_dir.RIGHT;
+				element++;
+			}
+			if(Input.GetKey(KeyCode.S))
+			{
+				ret[element]=walk_dir.DOWN;
+				element++;
+			}
+			if(element!=4)
+				Array.Resize(ref ret,element);
+			return ret;
+		}
+		void move(walk_dir[] w_dir)
+		{
+			float x=tf.position.x,
+				y=tf.position.y;
+			
+			for(int u=0;u!=w_dir.Length;++u)
+			{
+				if(w_dir[u]==walk_dir.LEFT)
+					tf.position+=((new Vector3(-1, 0, 0))*Time.deltaTime*tf.localScale.x);
+
+				if(w_dir[u]==walk_dir.UP)
+					tf.position+=((new Vector3( 0, 1, 0))*Time.deltaTime*tf.localScale.x);
+
+				if(w_dir[u]==walk_dir.RIGHT)
+					tf.position+=((new Vector3( 1, 0, 0))*Time.deltaTime*tf.localScale.x);
+
+				if(w_dir[u]==walk_dir.DOWN)
+					tf.position+=((new Vector3( 0,-1, 0))*Time.deltaTime*tf.localScale.x);
+			}
+			
+			moving_dir.x=tf.position.x-x;
+			moving_dir.y=tf.position.y-y;
+		}
+	
+	#endregion
+	#region self_sprite_update
+		public override void spirite_update()
+		{
+			Vector2 v2 = Camera.main.ScreenToViewportPoint( Input.mousePosition );
+			v2.x-=0.5f;
+			v2.y-=0.5f;
+			float ang=(float)(Math.Acos(v2.normalized.x)*360/(Math.PI*2));
+			if(v2.y<0)
+				ang=-ang;
+			ang=(ang+360)%360;
+			tf.rotation=Quaternion.Euler(0,0,ang);
+		}
+	#endregion
+	#region attack_behavior
+		public static GameObject bullet_prefab;
+		
+		public int cur_weapon_id=0;
+		public Weapon_Manager weapon_m;
+		Weapon cur_weapon;
+		void SwitchWeapon()
+		{
+			if(Input.mouseScrollDelta.y!=0)
+			{
+				int WP_Count=weapon_m.GetWeaponAmount();
+				cur_weapon_id+=(int)(Input.mouseScrollDelta.y);
+				cur_weapon_id=(cur_weapon_id+WP_Count)%WP_Count;
+
+				//Instantiate Weapon to Scence
+				cur_weapon=weapon_m.SetUpWeaponInstance(cur_weapon_id,this.gameObject);
+			}
+		}
+		void shoot()
+		{
+			if(Input.GetMouseButton(0))
+			{
+				cur_weapon.Attack();
+			}
+		}
+	#endregion
+	#region collision_behavior
+		void OnCollisionStay2D(Collision2D collisionInfo)
+		{
+			
+			if(collisionInfo.gameObject.tag=="Npc")
+			{
+				sp.transform.position=collisionInfo.transform.position;
+				Npc interact_npc=collisionInfo.gameObject.GetComponent<Npc>();
+
+				if(interact_npc!=null&&Input.GetKeyDown(KeyCode.Space))
+					Fungus.Flowchart.BroadcastFungusMessage("interact:"+interact_npc.NpcName);
+			}
+		}
+		void OnTriggerStay2D(Collider2D collisionInfo)
+		{
+			if(collisionInfo.gameObject.tag=="item")
+			{
+				sp.transform.position=collisionInfo.transform.position;
+				
+				if(Input.GetKeyDown(KeyCode.Space))
+				{
+					if(collisionInfo.gameObject.GetComponent<Item>()!=null)
+					{
+						Debug.Log("picked up");
+						GameObject NewItem=collisionInfo.gameObject;
+						Destroy(collisionInfo.gameObject);
+					}
+					else
+					{
+						Debug.Log("can not picked up!!");
+					}
+				}
+			}
+		}
+	#endregion
+	
 	public GameObject sp;
-	public static GameObject bullet_prefab;
-	Vector2 moving_dir;
-	public int cur_weapon_id=0;
-	public Weapon_Manager weapon_m;
-	Weapon cur_weapon;
+	
 	void LoadPlayerData()
 	{
 		PlayerData.LoadPlayerData();
@@ -21,18 +147,7 @@ public class Player : Entity {
 		LoadPlayerData();
 		cur_weapon=weapon_m.SetUpWeaponInstance(cur_weapon_id,this.gameObject);
 	}
-	void SwitchWeapon()
-	{
-		if(Input.mouseScrollDelta.y!=0)
-		{
-			int WP_Count=weapon_m.GetWeaponAmount();
-			cur_weapon_id+=(int)(Input.mouseScrollDelta.y);
-			cur_weapon_id=(cur_weapon_id+WP_Count)%WP_Count;
-
-			//Instantiate Weapon to Scence
-			cur_weapon=weapon_m.SetUpWeaponInstance(cur_weapon_id,this.gameObject);
-		}
-	}
+	
 	// Update is called once per frame
 	void Update ()
 	{
@@ -41,87 +156,5 @@ public class Player : Entity {
 		spirite_update();
 		SwitchWeapon();
 	}
-	void shoot()
-	{
-		if(Input.GetMouseButton(0))
-		{
-			cur_weapon.Attack();
-		}
-	}
-	public override void spirite_update()
-	{
-		Vector2 v2 = Camera.main.ScreenToViewportPoint( Input.mousePosition );
-		v2.x-=0.5f;
-		v2.y-=0.5f;
-		float ang=(float)(Math.Acos(v2.normalized.x)*360/(Math.PI*2));
-		if(v2.y<0)
-			ang=-ang;
-		ang=(ang+360)%360;
-		tf.rotation=Quaternion.Euler(0,0,ang);
-	}
-	walk_dir[] playerControler()
-	{
-		walk_dir[] ret=new walk_dir[4];
-		int element=0;
-		if(Input.GetKey(KeyCode.A))
-		{
-			ret[element]=walk_dir.LEFT;
-			element++;
-		}
-		if(Input.GetKey(KeyCode.W))
-		{
-			ret[element]=walk_dir.UP;
-			element++;
-		}
-		if(Input.GetKey(KeyCode.D))
-		{
-			ret[element]=walk_dir.RIGHT;
-			element++;
-		}
-		if(Input.GetKey(KeyCode.S))
-		{
-			ret[element]=walk_dir.DOWN;
-			element++;
-		}
-		if(element!=4)
-			Array.Resize(ref ret,element);
-		return ret;
-	}
-	void move(walk_dir[] w_dir)
-	{
-		float x=tf.position.x,
-			  y=tf.position.y;
-		
-		for(int u=0;u!=w_dir.Length;++u)
-		{
-			if(w_dir[u]==walk_dir.LEFT)
-				tf.position+=((new Vector3(-1, 0, 0))*Time.deltaTime*tf.localScale.x);
-
-			if(w_dir[u]==walk_dir.UP)
-				tf.position+=((new Vector3( 0, 1, 0))*Time.deltaTime*tf.localScale.x);
-
-			if(w_dir[u]==walk_dir.RIGHT)
-				tf.position+=((new Vector3( 1, 0, 0))*Time.deltaTime*tf.localScale.x);
-
-			if(w_dir[u]==walk_dir.DOWN)
-				tf.position+=((new Vector3( 0,-1, 0))*Time.deltaTime*tf.localScale.x);
-		}
-		
-		moving_dir.x=tf.position.x-x;
-		moving_dir.y=tf.position.y-y;
-	}
-	void OnCollisionStay2D(Collision2D collisionInfo)
-	{
-
-		if(collisionInfo.gameObject.tag=="Npc")
-		{
-			sp.transform.position=collisionInfo.transform.position;
-		}
-			
-
-		Npc interact_npc=collisionInfo.gameObject.GetComponent<Npc>();
-;
-		if(interact_npc!=null&&Input.GetKeyDown(KeyCode.Space))
-			Fungus.Flowchart.BroadcastFungusMessage("interact:"+interact_npc.NpcName);
-	}
+	
 }
