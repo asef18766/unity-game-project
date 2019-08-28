ebfdb1d3590a00d520986483e5f51e0bf2f5a4e5
Bogay <pojay11523@gmail.com>
2019-08-28 19:53:21 +0800

    move item scripts to Item folder

--- Assets/Scripts/Item/I_ItemUsage.cs
+++ Assets/Scripts/Item/I_ItemUsage.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-abstract class I_ItemUsage: ScriptableObject
+abstract class I_ItemUsage : ScriptableObject
 {
     abstract public void use();
 }
