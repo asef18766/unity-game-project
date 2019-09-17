aae839ef6763b95c972b58e9a0563e495ed21f4e
Bogay <pojay11523@gmail.com>
2019-09-17 21:13:01 +0800

    simple bag system, only partial interface completed

---
+++ Assets/Scripts/Item/Bag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Bag : MonoBehaviour
+{
+	private Dictionary<int, ItemCount> content;
+
+	void Start()
+	{
+		content = new Dictionary<int, ItemCount>();
+	}
+
+	public List<ItemCount> getItemList()
+	{
+		return content.Values.ToList();
+	}
+
+	// insert n items into bag
+	public bool updateItem(Item other, int n)
+	{
+		if(content.ContainsKey(other.getId()))
+			return content[other.getId()].updateCount(n);
+		else
+		{
+			ItemCount ic = ScriptableObject.CreateInstance<ItemCount>();
+			// set item
+			ic.updateCount(n);
+			content.Add(ic.id, ic);
+
+			return true;
+		}
+	}
+}
---
+++ Assets/Scripts/Item/ItemCount.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCount : ScriptableObject
+{
+	public const int MAX_COUNT = 99;
+
+	public int count { private set; get; }
+	public int id { get { return item.getId(); } }
+	private Item item;
+
+
+	public bool same(ItemCount other)
+	{
+		return item.getId() == other.id;
+	}
+
+	public bool use()
+	{
+		if(item == null || count == 0)
+		{
+			return false;
+		}
+
+		item.use_item();
+		count--;
+
+		// TODO: item empty event
+
+		return true;
+	}
+
+	public bool updateCount(int delta)
+	{
+		int newCount = count + delta;
+
+		// invalid count
+		if(newCount < 0 || newCount > MAX_COUNT) return false;
+
+		// update
+		count = newCount;
+		return true;
+	}
+}
