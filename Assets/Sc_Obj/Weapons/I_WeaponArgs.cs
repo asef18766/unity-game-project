774f871d561f5f9bdd09f389bad67475e12a2159
asef18766 <asef18766@gmail.com>
2019-11-23 20:26:10 +0800

    fix naming error

--- Assets/Sc_Obj/Weapons/I_WeaponArgs.cs
+++ Assets/Sc_Obj/Weapons/I_WeaponArgs.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Collections;
-public abstract class I_Weapon_Arg:ScriptableObject
+public abstract class I_WeaponArgs:ScriptableObject
 {
     public int dmg;
     public float reload;
     public float ammo_Capasity;
-    public float ammo_remain;
-    public I_AtkMethod atk_method;
+    public float ammoRemain;
+    public I_AtkMethod atkMethod;
 }
