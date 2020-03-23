17fc01c980ccba87464f2ad1a59ef515224b73b0
Bogay <pojay11523@gmail.com>
2020-03-24 03:22:12 +0800

    Implement fish unit

---
+++ Assets/Scripts/mini-game/fishing/Fish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniGame.Fishing
+{
+	public class Fish : MonoBehaviour
+	{
+		public FishData fishData;
+		public int upperBound { get { return this.fishData.upperBound; } }
+		public int lowerBound { get { return this.fishData.lowerBound; } }
+		public float duration { get { return this.fishData.duration; } }
+		// current hp 
+		public int hp;
+
+		private void Start()
+		{
+			this.hp = this.fishData.hp;
+		}
+	}
+}
---
+++ Assets/Scripts/mini-game/fishing/FishData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniGame.Fishing
+{
+	[CreateAssetMenu(fileName = "new Fish", menuName = "Mini game/fishing/Fish")]
+	public class FishData : ScriptableObject
+	{
+		public int upperBound;
+		public int lowerBound;
+		public float duration;
+		// max hp
+		public int hp;
+		public Sprite sprite;
+	}
+}
