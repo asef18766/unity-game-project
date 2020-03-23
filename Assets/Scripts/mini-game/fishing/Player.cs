31237c64a814014b8e0474b076a06d26df26fea6
Bogay <pojay11523@gmail.com>
2020-03-24 03:23:00 +0800

    Add player logic

---
+++ Assets/Scripts/mini-game/fishing/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniGame.Fishing
+{
+	public class Player : MonoBehaviour
+	{
+		public KeyCode hit;
+		private int fishCount;
+		private int score;
+
+		// get a new fish
+		public void GetFish(Fish fish)
+		{
+			this.fishCount++;
+			FishData fd = fish.fishData;
+			this.score += fd.hp * (fd.upperBound - fd.lowerBound);
+		}
+
+		public bool Hit()
+		{
+			// Debug.Log("Hit!");
+			return Input.GetKeyDown(this.hit);
+		}
+	}
+}
